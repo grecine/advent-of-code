fd8e95b65c28451f83d57e4ad2162afaaef962cd
Greg Recine <grecine@gmail.com>
2022-12-05 20:54:05 -0500

    added tools lib to templates

--- template/day.py
+++ template/day.py
@@ -1,21 +1,10 @@
 import numpy as np
 import os
 import time
+import aoc_tools.utils
 
 np.set_printoptions(threshold=np.inf)
 
-def input(fname):
-    day_dir = os.path.realpath(__file__).split('/')[:-1]
-    fname = os.path.join('/',*day_dir, fname)
-
-    data = []
-    with open(fname) as f:
-        for line in f:
-            data.append(line.strip())
-    return data        
-
-# data = input('input.txt')
-data = input('test-input.txt')
 
 def part1(data):
     # do stuff
@@ -25,14 +14,23 @@ def part2(data):
     # do stuff
     pass
 
-# part 1
-t0 = time.time()
-print('Part 1:')
-part1(data)
-print('Elapsed time:',time.time()-t0,' sec')
 
-# part 2
-t0 = time.time()
-print('\nPart 2:')
-part2(data)
-print('Elapsed time:',time.time()-t0,' sec')
+def main():
+    # data = aoc_tools.utils.read_input('input.txt')
+    data = aoc_tools.utils.read_input('test-input.txt')
+
+    # part 1
+    t0 = time.time()
+    print('Part 1:')
+    part1(data)
+    print('Elapsed time:',time.time()-t0,' sec')
+
+    # part 2
+    t0 = time.time()
+    print('\nPart 2:')
+    part2(data)
+    print('Elapsed time:',time.time()-t0,' sec')
+
+if __name__ == "__main__":
+    main()
+
