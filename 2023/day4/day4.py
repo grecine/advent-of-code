379e039cddd82c30e79da830c30050f3b71b38e1
Greg Recine <grecine@gmail.com>
2023-12-05 22:49:53 -0500

    getting caught up

--- 2023/day4/day4.py
+++ 2023/day4/day4.py
@@ -6,29 +6,68 @@ import aoctools.utils
 np.set_printoptions(threshold=np.inf)
 
 
-def part1(data):
-    # do stuff
-    pass
+def make_cards(data):
+    cards = {}
+    for d in data:
+        n = int(d.split(':')[0].split(' ')[-1])
+        nums = d.split(':')[1]
+        winning = nums.split('|')[0].strip().split(' ')
+        winning = [x for x in winning if x != '']
+        mine = nums.split('|')[1].strip().split(' ')
+        mine = [x for x in mine if x != '']
 
-def part2(data):
-    # do stuff
-    pass
+        copies = 1
 
+        cards[n] = [winning, mine, copies]
+
+    return cards
+
+def part1(cards):
+    total = 0
+    for n in cards:
+        winning = cards[n][0]
+        mine = cards[n][1]
+
+        matches = [x for x in mine if x in winning]
+        points = int(2**(len(matches)-1))
+
+        total += points
+
+    print(total)
+
+def part2(cards):
+    for n in cards:
+        for i in range(cards[n][2]):
+            winning = cards[n][0]
+            mine = cards[n][1]
+
+            matches = [x for x in mine if x in winning]
+
+            for j in range(len(matches)):
+                cards[n+(j+1)][2] += 1
+
+    tot = 0
+    for n in cards:
+        tot += cards[n][2]
+
+    print(tot)
 
 def main():
-    # data = aoctools.utils.read_input('input.txt')
-    data = aoctools.utils.read_input('test-input.txt')
+    data = aoctools.utils.read_input('input.txt')
+    # data = aoctools.utils.read_input('test-input.txt')
+
+    cards = make_cards(data)
 
     # part 1
     t0 = time.time()
     print('Part 1:')
-    part1(data)
+    part1(cards)
     print('Elapsed time:',(time.time()-t0)*1E3,' ms')
 
     # part 2
     t0 = time.time()
     print('\nPart 2:')
-    part2(data)
+    part2(cards)
     print('Elapsed time:',(time.time()-t0)*1E3,' ms')
 
 if __name__ == "__main__":
--- 2023/day5/day5.py
+++ 2023/day5/day5.py
@@ -1,34 +1,114 @@
 import numpy as np
-import os
+from tqdm.auto import tqdm
 import time
 import aoctools.utils
 
 np.set_printoptions(threshold=np.inf)
 
 
-def part1(data):
-    # do stuff
-    pass
+def make_map(data):
+    keys = [
+        'seeds',
+        'seed-to-soil map',
+        'soil-to-fertilizer map',
+        'fertilizer-to-water map',
+        'water-to-light map',
+        'light-to-temperature map',
+        'temperature-to-humidity map',
+        'humidity-to-location map',
+    ]
 
-def part2(data):
-    # do stuff
-    pass
+    seeds = tmp = []
+    for x in data:
+        tmp.extend(x.split(':'))
+    tmp = [x.strip() for x in tmp if x != '']
+
+    key_indexes = [tmp.index(k) for k in keys]
+    new_map = {}
+    for i in range(len(key_indexes)):
+        ikey = key_indexes[i]
+        ibeg = key_indexes[i]+1
+        iend = key_indexes[i+1] if i+1 < len(key_indexes) else None
+        if tmp[ikey] == 'seeds':
+            seeds = [int(i.strip()) for i in tmp[ibeg:iend][0].split(' ')]
+        else:
+            new_map[tmp[ikey]] = tmp[ibeg:iend]
+
+    for k in new_map:
+        for i in range(len(new_map[k])):
+            l = new_map[k][i].split(' ')
+            rng = int(l[2])
+            dest = int(l[0])
+            src = int(l[1])
+            new_map[k][i] = [dest, src, rng]
+
+    keys.remove('seeds')
+
+    return seeds, keys, new_map
+
+
+def do_map(n, k, elf_map):
+    m = elf_map[k]
+    mapped_n = n
+    for l in m:
+        dest = l[0]
+        src = l[1]
+        rng = l[2]
+        if src <= n < src+rng:
+            mapped_n = dest + (n-src)
+
+    return mapped_n
+
+
+def part1(seeds, keys, elf_map):
+    location = []
+    for s in seeds:
+        n = s
+        for k in keys:
+            n = do_map(n, k, elf_map)
+        location.append(n)
+
+    print(min(location))
+
+
+def part2(seeds, keys, elf_map):
+    location = []
+    for i in range(int(len(seeds)/2)):
+        n = seeds[2*i]
+        for k in keys:
+            n = do_map(n, k, elf_map)
+        location.append((n,i))
+
+    min_loc = min(location)[0]
+    i = min(location)[1]
+
+    new_seeds = [seeds[2*i]+j for j in range(seeds[2*i+1])]
+
+    for s in tqdm(new_seeds):
+        n = s
+        for k in keys:
+            n = do_map(n, k, elf_map)
+        min_loc = min(min_loc, n)
+
+    print(min_loc)
 
 
 def main():
-    # data = aoctools.utils.read_input('input.txt')
-    data = aoctools.utils.read_input('test-input.txt')
+    data = aoctools.utils.read_input('input.txt')
+    # data = aoctools.utils.read_input('test-input.txt')
+
+    seeds, keys, elf_map = make_map(data)
 
     # part 1
     t0 = time.time()
     print('Part 1:')
-    part1(data)
+    part1(seeds, keys, elf_map)
     print('Elapsed time:',(time.time()-t0)*1E3,' ms')
 
     # part 2
     t0 = time.time()
     print('\nPart 2:')
-    part2(data)
+    part2(seeds, keys, elf_map)
     print('Elapsed time:',(time.time()-t0)*1E3,' ms')
 
 if __name__ == "__main__":
